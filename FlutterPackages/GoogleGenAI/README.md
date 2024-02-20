be283e8e0e54ef2b72efe33e01e24b533df9883a
Ravi Patel <63658662+ravipatel0508@users.noreply.github.com>
2024-02-20 15:54:56 +0530

    Addedd GoogleGenAI example

---
+++ FlutterPackages/GoogleGenAI/README.md
@@ -0,0 +1,5 @@
+## GoogleGenAI with Flutter
+
+This is ultimate package for GoogleGenerativeAI - Gemini.
+
+### [Click here to see the output](https://www.instagram.com/reel/C3kKKhnNbbp/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==)
---
+++ FlutterPackages/GoogleGenAI/main.dart
@@ -0,0 +1,277 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const GenerativeAISample(title: 'Flutter + Generative AI'),
+    );
+  }
+}
+
+class GenerativeAISample extends StatelessWidget {
+  const GenerativeAISample({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: Colors.green[100],
+      ),
+      body: const ChatWidget(),
+    );
+  }
+}
+
+class ChatWidget extends StatefulWidget {
+  const ChatWidget({super.key});
+
+  @override
+  State<ChatWidget> createState() => _ChatWidgetState();
+}
+
+class _ChatWidgetState extends State<ChatWidget> {
+  late final GenerativeModel _model;
+  late final ChatSession _chat;
+  final ScrollController _scrollController = ScrollController();
+  final TextEditingController _textController = TextEditingController();
+  final FocusNode _textFieldFocus = FocusNode();
+  bool _loading = false;
+  // static const _apiKey =
+  //     String.fromEnvironment('GENERATIVE_AI_API_KEY');
+  static const _apiKey = "YOUR_API_KEY";
+
+  @override
+  void initState() {
+    super.initState();
+    _model = GenerativeModel(
+      model: 'gemini-pro',
+      apiKey: _apiKey,
+    );
+    _chat = _model.startChat();
+  }
+
+  void _scrollDown() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(milliseconds: 100), () {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: const Duration(
+            milliseconds: 750,
+          ),
+          curve: Curves.easeOutCirc,
+        );
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var textFieldDecoration = InputDecoration(
+      contentPadding: const EdgeInsets.all(15),
+      hintText: 'Enter a prompt...',
+      hintStyle: TextStyle(
+        color: Theme.of(context).colorScheme.secondary,
+      ),
+      border: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(14),
+        ),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(Radius.circular(14)),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: _apiKey.isNotEmpty
+                ? ListView.builder(
+                    controller: _scrollController,
+                    itemBuilder: (context, idx) {
+                      var content = _chat.history.toList()[idx];
+                      var text = content.parts
+                          .whereType<TextPart>()
+                          .map<String>((e) => e.text)
+                          .join('');
+                      return MessageWidget(
+                        text: text,
+                        isFromUser: content.role == 'user',
+                      );
+                    },
+                    itemCount: _chat.history.length,
+                  )
+                : ListView(
+                    children: const [
+                      Text('No API key found. Please provide an API Key.'),
+                    ],
+                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    // autofocus: true,
+                    focusNode: _textFieldFocus,
+                    decoration: textFieldDecoration,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    controller: _textController,
+                    onSubmitted: (String value) {
+                      _sendChatMessage(value);
+                      _textFieldFocus.unfocus();
+                    },
+                  ),
+                ),
+                // const SizedBox.square(dimension: 5),
+                if (!_loading)
+                  IconButton(
+                    onPressed: () async {
+                      _sendChatMessage(_textController.text);
+                      _textFieldFocus.unfocus();
+                    },
+                    icon: Icon(
+                      Icons.send,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  )
+                else
+                  const Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: CircularProgressIndicator(),
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _sendChatMessage(String message) async {
+    setState(() {
+      _loading = true;
+    });
+
+    try {
+      var response = await _chat.sendMessage(
+        Content.text(message),
+      );
+      var text = response.text;
+
+      if (text == null) {
+        _showError('No response from API.');
+        return;
+      } else {
+        setState(() {
+          _loading = false;
+          _scrollDown();
+        });
+      }
+    } catch (e) {
+      _showError(e.toString());
+      setState(() {
+        _loading = false;
+      });
+    } finally {
+      _textController.clear();
+      setState(() {
+        _loading = false;
+      });
+      // _textFieldFocus.requestFocus();
+    }
+  }
+
+  void _showError(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Something went wrong'),
+          content: SingleChildScrollView(
+            child: SelectableText(message),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
+
+class MessageWidget extends StatelessWidget {
+  final String text;
+  final bool isFromUser;
+
+  const MessageWidget({
+    super.key,
+    required this.text,
+    required this.isFromUser,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment:
+          isFromUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Flexible(
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 600),
+            decoration: BoxDecoration(
+              color: isFromUser
+                  ? Theme.of(context).colorScheme.primaryContainer
+                  : Theme.of(context).colorScheme.surfaceVariant,
+              borderRadius: BorderRadius.circular(18),
+            ),
+            padding: const EdgeInsets.symmetric(
+              vertical: 15,
+              horizontal: 20,
+            ),
+            margin: const EdgeInsets.only(top: 10, bottom: 10),
+            child: MarkdownBody(
+              selectable: true,
+              data: text,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
