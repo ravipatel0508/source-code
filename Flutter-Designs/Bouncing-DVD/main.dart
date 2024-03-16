5becefec223174bdd3d3cebaa45db8447f9f7a09
Ravi Patel <63658662+ravipatel0508@users.noreply.github.com>
2024-03-16 18:05:29 +0530

    Add: Bouncing-DVD-Design

---
+++ Flutter-Designs/Bouncing-DVD/main.dart
@@ -0,0 +1,121 @@
+//
+// Bouncing DVD
+//
+// Created by: Ravi Patel
+// Instagram: @bugs_fixes (https://www.instagram.com/bugs_fixes)
+//
+
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.deepPurple,
+      ),
+      home: const BouncingDVD(),
+    );
+  }
+}
+
+class BouncingDVD extends StatefulWidget {
+  const BouncingDVD({Key? key}) : super(key: key);
+
+  @override
+  State<BouncingDVD> createState() => _BouncingDVDState();
+}
+
+class _BouncingDVDState extends State<BouncingDVD> {
+  Random random = Random();
+  Color dvdColor = Colors.pink;
+  double dvdWidth = 150, dvdHeight = 80;
+  double x = 90, y = 30, xSpeed = 50, ySpeed = 50, speed = 150;
+
+  pickColor() {
+    // removing the timer will make the dvd color change before bouncing
+    // off the edge
+    Timer(const Duration(milliseconds: 100), () {
+      int r = random.nextInt(255);
+      int g = random.nextInt(255);
+      int b = random.nextInt(255);
+      dvdColor = Color.fromRGBO(r, g, b, 1);
+    });
+  }
+
+  @override
+  initState() {
+    super.initState();
+    update();
+  }
+
+  update() {
+    Timer.periodic(Duration(milliseconds: speed.toInt()), (timer) {
+      double screenWidth = MediaQuery.of(context).size.width;
+      double screenHeight = MediaQuery.of(context).size.height;
+      x += xSpeed;
+      y += ySpeed;
+
+      if (x + dvdWidth >= screenWidth) {
+        xSpeed = -xSpeed;
+        x = screenWidth - dvdWidth;
+        pickColor();
+      } else if (x <= 0) {
+        xSpeed = -xSpeed;
+        x = 0;
+        pickColor();
+      }
+
+      if (y + dvdHeight >= screenHeight) {
+        ySpeed = -ySpeed;
+        y = screenHeight - dvdHeight;
+        pickColor();
+      } else if (y <= 0) {
+        ySpeed = -ySpeed;
+        y = 0;
+        pickColor();
+      }
+
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.white,
+        title: const Text('Bouncing DVD'),
+      ),
+      body: Stack(
+        children: [
+          AnimatedPositioned(
+            duration: Duration(milliseconds: speed.toInt()),
+            left: x,
+            top: y,
+            child: Image.asset(
+              'assets/dvd-logo.png',
+              color: dvdColor,
+              height: dvdHeight,
+              width: dvdWidth,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
