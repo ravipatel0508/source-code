03aecc1fa5f7a1e3a9b3bfba8ee692603dc89b7f
Ravi Patel <63658662+ravipatel0508@users.noreply.github.com>
2024-04-12 12:27:39 +0530

    Update Sharing in Flutter

--- Flutter-Packages/Share/main.dart
+++ Flutter-Packages/Share/main.dart
@@ -25,6 +25,7 @@ class SharingExample extends StatelessWidget {
                 const SizedBox(height: 20),
                 TextFormField(
                   controller: subjectController,
+                  textCapitalization: TextCapitalization.sentences,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Please enter some text';
@@ -33,12 +34,15 @@ class SharingExample extends StatelessWidget {
                   },
                   decoration: const InputDecoration(
                     label: Text('Subject'),
-                    border: OutlineInputBorder(),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(14.0)),
+                    ),
                   ),
                 ),
                 const SizedBox(height: 20),
                 TextFormField(
                   controller: controller,
+                  textCapitalization: TextCapitalization.sentences,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Please enter some text';
@@ -47,22 +51,45 @@ class SharingExample extends StatelessWidget {
                   },
                   decoration: const InputDecoration(
                     label: Text('Text'),
-                    border: OutlineInputBorder(),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(14.0)),
+                    ),
                   ),
                 ),
                 const SizedBox(height: 20),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueGrey,
+                    backgroundColor: Colors.green[200],
                     foregroundColor: Colors.white,
                     fixedSize: const Size(200, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                    ),
                   ),
                   onPressed: () async {
                     if (_formKey.currentState!.validate()) {
-                      return Share.share(controller.text, subject: subjectController.text);
+                      // return Share.share(controller.text,
+                      //     subject: subjectController.text);
+                      final result =
+                          await Share.shareWithResult(controller.text);
+
+                      if (result.status == ShareResultStatus.success) {
+                        if (context.mounted) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              behavior: SnackBarBehavior.floating,
+                              backgroundColor: Colors.green[300],
+                              content: const Text('Thank you for sharing!',
+                                  style: TextStyle(color: Colors.black),
+                                  textAlign: TextAlign.center),
+                            ),
+                          );
+                        }
+                      }
                     }
                   },
-                  child: const Text('Share'),
+                  child: const Text('Share',
+                      style: TextStyle(fontSize: 20, color: Colors.black)),
                 ),
               ],
             ),
